7d84a96ae93fe7fcfd68f274775fa04b4c09861c
aschlierca <chosseo.yeon@gmail.com>
2026-06-17 10:35:08 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  let name = ''
+  return (
+    <div>
+      <h1>RSVP Practice</h1><input type="text" />
+    </div>
+  )
+}
