57679f19013fe49d1199bf03182d30ec1f1aa1d4
aschlierca <chosseo.yeon@gmail.com>
2026-06-17 10:45:53 -0400

    Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  let name = ''
+  const [name, setName] = useState("");
   return (
     <div>
-      <h1>RSVP Practice</h1><input type="text" />
+      <h1>RSVP Practice</h1>
+
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => setName(event.target.value)} />
+      
+      <p>{name}</p>
     </div>
   )
 }
