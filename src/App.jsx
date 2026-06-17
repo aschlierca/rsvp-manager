31c2ae0d00797ace67979f5ded16093a07166679
aschlierca <chosseo.yeon@gmail.com>
2026-06-17 10:54:27 -0400

    Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,36 @@ import './App.css'
 
 export default function App() {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div>
-      <h1>RSVP Practice</h1>
+      <h1>Company Picninc RSVP</h1>
 
-      <input
-        type="text"
-        value={name}
-        onChange={(event) => setName(event.target.value)} />
-      
-      <p>{name}</p>
+      <form>
+        <label>
+          Name
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Email
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <button type="submit">Add Guest</button>
+      </form>
+
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
     </div>
   )
 }
